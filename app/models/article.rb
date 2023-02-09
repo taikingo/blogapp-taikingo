f92b3df6103f589c8edef7d4504d3d4b946adf23
tkbonz <>
2023-02-09 15:39:00 +0900

    ユーザに紐付いた記事を作成できるように変更 validation の content の文字数を100から10文字以上に変更

--- app/models/article.rb
+++ app/models/article.rb
@@ -33,8 +33,8 @@ class Article < ApplicationRecord
   private
   def validate_title_and_content_length
     char_count = self.title.length + self.content.length
-    unless char_count > 100
-      errors.add(:content, '100文字以上で')
+    unless char_count > 10
+      errors.add(:content, '10文字以上で')
     end
   end
 
